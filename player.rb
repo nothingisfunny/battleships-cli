0c848fa64d566229758c6d071a156563d6148ced
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-04-29 18:19:07 -0700

    add player class

---
+++ player.rb
@@ -0,0 +1,13 @@
+class Player
+  attr_accessor :board, :fleet
+
+  def initialize()
+    self.fleet = [Carrier.new(), 
+                  Battleship.new(), 
+                  Cruiser.new(), Cruiser.new(), 
+                  Destroyer.new(), Destroyer.new(),
+                  Submarine.new(), Submarine.new(), Submarine.new()]
+    
+  end
+end
+
